2b6a0a707a43a12d634746b8b4423e0c4bc842ab
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-02-19 18:36:54 +0500

    learn about oops

---
+++ oops/main.py
@@ -0,0 +1,23 @@
+# Defining a class (Encapsulation)
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Method to display details (Abstraction)
+    def get_details(self):
+        return f"{self.name} is {self.age} years old."
+
+# Inheritance - Student inherits from Person
+class Student(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)  # Calling parent constructor
+        self.grade = grade
+
+    # Overriding method (Polymorphism)
+    def get_details(self):
+        return f"{self.name} is {self.age} years old and in grade {self.grade}."
+
+# Creating objects
+student1 = Student("Abdullah", 18, "A")
+print(student1.get_details())  # Output: Abdullah is 18 years old and in grade A.
